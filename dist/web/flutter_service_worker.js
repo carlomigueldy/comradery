780bd0e363ec928c3b85760f55ae6107146cf0a3
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-07-31 20:16:44 +0800

    Removed cupertino icon

--- dist/web/flutter_service_worker.js
+++ dist/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "0e8dfb41818e17d8bc958c5d227e8f87",
+  "assets/AssetManifest.json": "af78c9686bf994971bfd168a01f01bc6",
 "assets/assets/fonts/Sora/OFL.txt": "6ed93967ff0dc6dd9c8d31c17f817a06",
 "assets/assets/fonts/Sora/Sora-Bold.ttf": "8b24edfb87ce7942c970dbaeaa73a511",
 "assets/assets/fonts/Sora/Sora-ExtraBold.ttf": "23b29030a22fc02a05c8ca5d2cda8bc5",
@@ -13,15 +13,14 @@ const RESOURCES = {
 "assets/assets/fonts/Sora/Sora-Regular.ttf": "aa64200d21f88f287becbe3f3cebaf0e",
 "assets/assets/fonts/Sora/Sora-SemiBold.ttf": "fb54a7a5fdc54d9385f3f42421083ca4",
 "assets/assets/fonts/Sora/Sora-Thin.ttf": "c41ed373cfbf85cd6deb945dbd4996c0",
-"assets/FontManifest.json": "1b485297a76a084a4688a0b0e7357d50",
+"assets/FontManifest.json": "2cc6cc75f2d817db8687e279fc3b4950",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "6efa5c1092315d2c2e0152932ff30cd8",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"assets/NOTICES": "faaa78f9ad0cd7bf00e4fc755b9e2019",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "d562712aef5e1064a2c3da173e8296fc",
-"/": "d562712aef5e1064a2c3da173e8296fc",
+"index.html": "690390f5823dff2433b3300db54dc768",
+"/": "690390f5823dff2433b3300db54dc768",
 "main.dart.js": "3e49dc9071e419884c3718dfee36526c",
 "manifest.json": "55076722cfa659413d1df2011cc3ff43",
 "version.json": "dd97e380589dc2a1be5d14b75d362396"
