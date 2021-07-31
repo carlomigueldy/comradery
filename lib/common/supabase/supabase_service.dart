e8773da2491c62742bfef52290c16611858bbb31
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-07-31 19:07:02 +0800

    Added supabase client and supabase service

---
+++ lib/common/supabase/supabase_service.dart
@@ -0,0 +1,69 @@
+import 'package:logger/logger.dart';
+import 'package:postgrest/postgrest.dart';
+
+import 'supabase_client.dart';
+
+abstract class SupabaseService<T> {
+  Logger get log => Logger();
+
+  String get table;
+
+  Future<PostgrestResponse> all() async {
+    log.i('table "$table"');
+    final response =
+        await supabase.from(table).select().is_('deleted_at', null).execute();
+    log.i(response.toJson());
+    return response;
+  }
+
+  Future<PostgrestResponse> find(String id) async {
+    log.i('table "$table", id "$id"');
+    final response = await supabase
+        .from(table)
+        .select()
+        .is_('deleted_at', null)
+        .eq('id', id)
+        .single()
+        .execute();
+    return response;
+  }
+
+  Future<PostgrestResponse> create(Map<String, dynamic> json) async {
+    log.i('table "$table", json "$json"');
+    final response = await supabase.from(table).insert(json).execute();
+    return response;
+  }
+
+  Future<PostgrestResponse> update({
+    required String id,
+    required Map<String, dynamic> json,
+  }) async {
+    log.i('table "$table", json "$json"');
+    final response = await supabase
+        .from(table)
+        .update(json)
+        .is_('deleted_at', null)
+        .eq('id', id)
+        .execute();
+    return response;
+  }
+
+  Future<PostgrestResponse> delete(String id) async {
+    log.i('table "$table", id "$id"');
+    final response = await supabase
+        .from(table)
+        .update({
+          'deleted_at': DateTime.now().toIso8601String(),
+        })
+        .is_('deleted_at', null)
+        .eq('id', id)
+        .execute();
+    return response;
+  }
+
+  // Future<PostgrestResponse> forceDelete(String id) async {
+  //   log.i('table "$table", id "$id"');
+  //   final response = await supabase.from(table).delete().eq('id', id).execute();
+  //   return response;
+  // }
+}
