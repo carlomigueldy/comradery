4e049e15053de542c271d77cbb9360c6b016e256
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-08-01 19:53:37 +0800

    'Build'

--- dist/flutter_service_worker.js
+++ dist/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "3df9e4705e0f1fdb4b078139ba6c74c9",
+  "assets/AssetManifest.json": "f3c397e7bff7034e1765b2fe55e020e0",
 "assets/assets/fonts/Sora/OFL.txt": "6ed93967ff0dc6dd9c8d31c17f817a06",
 "assets/assets/fonts/Sora/Sora-Bold.ttf": "8b24edfb87ce7942c970dbaeaa73a511",
 "assets/assets/fonts/Sora/Sora-ExtraBold.ttf": "23b29030a22fc02a05c8ca5d2cda8bc5",
@@ -13,6 +13,8 @@ const RESOURCES = {
 "assets/assets/fonts/Sora/Sora-Regular.ttf": "aa64200d21f88f287becbe3f3cebaf0e",
 "assets/assets/fonts/Sora/Sora-SemiBold.ttf": "fb54a7a5fdc54d9385f3f42421083ca4",
 "assets/assets/fonts/Sora/Sora-Thin.ttf": "c41ed373cfbf85cd6deb945dbd4996c0",
+"assets/dist/assets/dist/assets/dist/assets/dotenv": "9afc40884076b132de2a821f49079c61",
+"assets/dist/assets/dist/assets/dotenv": "9afc40884076b132de2a821f49079c61",
 "assets/dist/assets/dotenv": "9afc40884076b132de2a821f49079c61",
 "assets/dotenv": "9afc40884076b132de2a821f49079c61",
 "assets/FontManifest.json": "2cc6cc75f2d817db8687e279fc3b4950",
@@ -21,9 +23,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "ef5ce3e82e7c52c8590cb171ca9c55f0",
-"/": "ef5ce3e82e7c52c8590cb171ca9c55f0",
-"main.dart.js": "50ff480e474a2521a96ad9f1aa4bca71",
+"index.html": "3d7581671e94497ba2e96d0585b462f9",
+"/": "3d7581671e94497ba2e96d0585b462f9",
+"main.dart.js": "6c0f757aad05bfeadfe96f8436c58028",
 "manifest.json": "55076722cfa659413d1df2011cc3ff43",
 "version.json": "dd97e380589dc2a1be5d14b75d362396"
 };
